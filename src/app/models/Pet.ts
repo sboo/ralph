20efe0c5beee2476a6242d086cc78d02df656882
Ramon Nieves <ramon@uselab.com>
2024-05-04 19:07:11 +0200

    adds index to Pet

--- src/app/models/Pet.ts
+++ src/app/models/Pet.ts
@@ -1,13 +1,15 @@
-import Realm, {BSON} from 'realm';
+import Realm, {BSON, index} from 'realm';
 
 export class Pet extends Realm.Object {
   _id: BSON.ObjectId = new BSON.ObjectId();
   static primaryKey = '_id';
   species!: string;
+  @index('full-text')
   name!: string;
   avatar?: string;
   notificationsEnabled: boolean = false;
   notificationsTime?: string;
+  @index
   isActive: boolean = false;
   headerColor?: string;
 }
