f7f6ed8bee85c64ea181a58a087c8f8857606950
Ramon Nieves <ramon@uselab.com>
2024-05-04 11:41:58 +0200

    cleaned up event code

--- src/features/events/index.ts
+++ src/features/events/index.ts
@@ -2,6 +2,5 @@ import {EventEmitter} from 'eventemitter3';
 export const event = new EventEmitter();
 
 export enum EVENT_NAMES {
-  PROFILE_SET = 'profileSet',
   COFFEE_PURCHASED = 'coffeePurchased',
 }
