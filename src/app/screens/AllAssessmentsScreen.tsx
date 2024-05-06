a853818f57e59f581a75d793905139afec35ae20
Ramon Nieves <ramon@uselab.com>
2024-05-07 01:22:55 +0200

    changed screen navigation

--- src/app/screens/AllAssessmentsScreen.tsx
+++ src/app/screens/AllAssessmentsScreen.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {useTranslation} from 'react-i18next';
 import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import {Divider, useTheme} from 'react-native-paper';
 import {AllAssessmentsScreenNavigationProps} from '@/features/navigation/types.tsx';
@@ -7,32 +6,15 @@ import LinearGradient from 'react-native-linear-gradient';
 import ExportPdf from '@/features/pdfExport/components/ExportPdf';
 import useAssessments from '@/features/assessments/hooks/useAssessments';
 import usePet from '@/features/pets/hooks/usePet';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
-import moment from 'moment';
-import {DateData, MarkedDates} from 'react-native-calendars/src/types';
+import {DateData} from 'react-native-calendars/src/types';
+import AssessmentsCalendar from '@/features/assessments/components/AssessmentsCalendar';
 
 const AllAssessmentsScreen: React.FC<AllAssessmentsScreenNavigationProps> = ({
   navigation,
 }) => {
-  const {t, i18n} = useTranslation();
   const theme = useTheme();
   const {activePet} = usePet();
   const {lastAssessments} = useAssessments(activePet);
-  LocaleConfig.defaultLocale = i18n.resolvedLanguage;
-
-  const getIconColor = (score: number) => {
-    if (score < 6) {
-      return '#F44336';
-    } else if (score < 15) {
-      return '#F49503';
-    } else if (score < 30) {
-      return '#F0E106';
-    } else if (score < 45) {
-      return '#74D400';
-    } else {
-      return '#4CAF50';
-    }
-  };
 
   const addOrEditAssessment = (dateData: DateData) => {
     const assessment = lastAssessments?.find(
@@ -49,16 +31,6 @@ const AllAssessmentsScreen: React.FC<AllAssessmentsScreenNavigationProps> = ({
     }
   };
 
-  const markedDates: MarkedDates =
-    lastAssessments?.reduce((acc, assessment) => {
-      const date = moment(assessment.createdAt).format('YYYY-MM-DD');
-      acc[date] = {
-        selected: true,
-        selectedColor: getIconColor(assessment.score),
-      };
-      return acc;
-    }, {} as MarkedDates) ?? ({} as MarkedDates);
-
   return (
     <SafeAreaView
       style={{
@@ -74,15 +46,7 @@ const AllAssessmentsScreen: React.FC<AllAssessmentsScreenNavigationProps> = ({
         locations={[0, 0.75, 1]}
         style={styles.gradient}>
         <ScrollView style={styles.scrollview}>
-          <Calendar
-            style={styles.calendar}
-            theme={{
-              arrowColor: theme.colors.primary,
-            }}
-            onDayPress={addOrEditAssessment}
-            markedDates={markedDates}
-            maxDate={moment().format('YYYY-MM-DD')}
-          />
+          <AssessmentsCalendar onCalendarDayPress={addOrEditAssessment} />
           <Divider style={styles.divider} bold={true} />
           <ExportPdf />
         </ScrollView>
---
+++ src/features/assessments/components/AssessmentsCalendar.tsx
@@ -0,0 +1,66 @@
+import usePet from '@/features/pets/hooks/usePet';
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+import {Calendar, DateData, LocaleConfig} from 'react-native-calendars';
+import {useTheme} from 'react-native-paper';
+import useAssessments from '../hooks/useAssessments';
+import {MarkedDates} from 'react-native-calendars/src/types';
+import moment from 'moment';
+import {StyleSheet} from 'react-native';
+
+interface Props {
+  onCalendarDayPress: (dateData: DateData) => void;
+}
+
+const AssessmentsCalendar: React.FC<Props> = ({onCalendarDayPress}) => {
+  const {i18n} = useTranslation();
+  const theme = useTheme();
+  const {activePet} = usePet();
+  const {lastAssessments} = useAssessments(activePet);
+  LocaleConfig.defaultLocale = i18n.resolvedLanguage;
+
+  const getIconColor = (score: number) => {
+    if (score < 6) {
+      return '#F44336';
+    } else if (score < 15) {
+      return '#F49503';
+    } else if (score < 30) {
+      return '#F0E106';
+    } else if (score < 45) {
+      return '#74D400';
+    } else {
+      return '#4CAF50';
+    }
+  };
+
+  const markedDates: MarkedDates =
+    lastAssessments?.reduce((acc, assessment) => {
+      const date = moment(assessment.createdAt).format('YYYY-MM-DD');
+      acc[date] = {
+        selected: true,
+        selectedColor: getIconColor(assessment.score),
+      };
+      return acc;
+    }, {} as MarkedDates) ?? ({} as MarkedDates);
+
+  return (
+    <Calendar
+      style={styles.calendar}
+      theme={{
+        arrowColor: theme.colors.primary,
+      }}
+      onDayPress={onCalendarDayPress}
+      markedDates={markedDates}
+      maxDate={moment().format('YYYY-MM-DD')}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  calendar: {
+    borderRadius: 15,
+    padding: 10,
+  },
+});
+
+export default AssessmentsCalendar;
